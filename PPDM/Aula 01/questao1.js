f4cc3c1e24e09e23851dade782ce3f1140359828
Augusto <143622825+Augustogutinho@users.noreply.github.com>
2024-07-19 15:07:17 -0300

    Add files via upload

--- PPDM/Aula 01/questao1.js
+++ PPDM/Aula 01/questao1.js
@@ -1,4 +1,44 @@
+/*
+1
 function teste(nome) {
     console.log(`Bom dia, ${nome}`)
 }
 teste (`Carlos`)
+*/
+
+function numeroParaMes(numero) {
+    switch(numero) {
+        case 1:
+            return "janeiro";
+        case 2:
+            return "fevereiro";
+        case 3:
+            return "março";
+        case 4:
+            return "abril";
+        case 5:
+            return "maio";
+        case 6:
+            return "junho";
+        case 7:
+            return "julho";
+        case 8:
+            return "agosto";
+        case 9:
+            return "setembro";
+        case 10:
+            return "outubro";
+        case 11:
+            return "novembro";
+        case 12:
+            return "dezembro";
+        default:
+            return "Mês inválido";
+    }
+}
+
+// Exemplos de uso:
+console.log(numeroParaMes(1)); // saída: "janeiro"
+console.log(numeroParaMes(4)); // saída: "abril"
+console.log(numeroParaMes(12)); // saída: "dezembro"
+console.log(numeroParaMes(13)); // saída: "Mês inválido"
