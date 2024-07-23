75d1a8e5b0e57a787d7eccbbd805a472835d768e
Augusto <augusto.souza@gsuite.iff.edu.br>
2024-07-23 09:49:24 -0300

    Atividade

--- PPDM/Aula 01/questao1.js
+++ PPDM/Aula 01/questao1.js
@@ -1,13 +1,14 @@
-/*
-1
+
+//1
 function teste(nome) {
     console.log(`Bom dia, ${nome}`)
 }
-teste (`Carlos`)
-*/
+teste(`Carlos`)
 
+
+//2
 function numeroParaMes(numero) {
-    switch(numero) {
+    switch (numero) {
         case 1:
             return "janeiro";
         case 2:
@@ -37,8 +38,41 @@ function numeroParaMes(numero) {
     }
 }
 
-// Exemplos de uso:
-console.log(numeroParaMes(1)); // saída: "janeiro"
-console.log(numeroParaMes(4)); // saída: "abril"
-console.log(numeroParaMes(12)); // saída: "dezembro"
-console.log(numeroParaMes(13)); // saída: "Mês inválido"
+console.log(numeroParaMes(1));
+console.log(numeroParaMes(4));
+console.log(numeroParaMes(12));
+console.log(numeroParaMes(13));
+
+//3
+function comparaNumero(x, y) {
+    if (x == NaN || y == NaN) {
+        console.log("Um ou mais dos valores não é um número");
+    }
+    else if (x > y) {
+        console.log("O primeiro número é maior que o segundo")
+    }
+    else if (x < y) {
+        console.log("O primeiro número é menor que o segundo")
+    }
+    else if (x == y) {
+        console.log("Numeros iguais");
+    }
+}
+comparaNumero(9, 8)
+//4
+
+function trab(salario, trabalho, faltas) {
+   let hora = salario / 176
+   let extra = hora * 1.50 * trabalho
+   let carga = hora * faltas * 8
+   let liquido = parseInt(salario) + parseInt(extra) - carga
+   console.log(`Salario ${parseInt(salario)} Salario liquido ${parseInt(liquido)} Total de horas extras ${parseInt(extra)} Quantidades de faltas ${parseInt(carga)}`);
+}
+trab (10000, 80, 50)
+
+//5
+const array = [2, 4, 67, 8, 9, 90, 0, "dia", "hoje", "tio", "yellow", "ui"]
+function quest5(array) {
+    console.log(`${array[0]} ${array[array.length - 1]}`)
+}
+quest5(array)
